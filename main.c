83f3d244147be50131b1412fd04d172a1488c789
Kostyantin1408 <88367873+Kostyantin1408@users.noreply.github.com>
2025-04-14 15:58:39 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 
 SEC("xdp")
 int xdp_hello(struct xdp_md *ctx) {
-    static const char fmt[] = "blyat";
-    bpf_trace_printk(fmt, 6);
+    static const char fmt[] = "yo";
+    bpf_trace_printk(fmt, 3);
     return XDP_PASS;
 }
 
